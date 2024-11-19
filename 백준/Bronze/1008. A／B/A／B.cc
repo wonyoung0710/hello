55bab8f40f593f6f40701663716847224fe5b4d6
wonyoung0710 <dnjs4023@bu.ac.kr>
2024-11-19 19:26:51 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    
+    cin >> a >> b;
+
+    cout << fixed;
+    cout.precision(9);
+    
+    cout << a/ static_cast<double>(b) << endl;
+
+
+    return 0;
+}
